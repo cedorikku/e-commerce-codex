42fc4bba1a19ceb78e6f387133d7e9b4a19e2b4c
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-07-17 23:34:11 +0800

    Make post method for product name

--- public/script.js
+++ public/script.js
@@ -3,5 +3,16 @@ const addToCartButton = document.querySelectorAll('.card-footer > button');
 addToCartButton.forEach(button => button.addEventListener('click', addToCart));
 
 function addToCart(e) {
-  console.log(e.target.parentNode.childNodes[1].textContent);
+  const productName = e.target.parentNode.childNodes[1].textContent;
+
+  fetch('/api/cart', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title: productName }),
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.error('Error:', error));
 }
