9a3f9c82a6024af14b6dd6dd4837ca256975dab9
xenri20 <skc1615@dlsud.edu.ph>
2024-07-18 19:28:27 +0800

    Added a subtotal function for each products in the cart

--- controller/controllers.js
+++ controller/controllers.js
@@ -6,13 +6,15 @@ const addToCart = async (req, res) => {
     const added = await Inventory.findByIdAndUpdate(item._id, { $inc: { stock: -1 } })
     let tempItem = await tempUserCart.findOne({name: item.name})
     if (tempItem) {
-        await tempUserCart.findOneAndUpdate(tempItem._id, { $inc: { qty: +1 } });
+        await tempUserCart.findOneAndUpdate({_id: tempItem._id}, { $inc: { qty: 1 }});
+        await tempUserCart.findOneAndUpdate({_id: tempItem._id}, { $inc: { subtotal: item.price }});
     } else {
         const newItem = new tempUserCart({
             name: item.name,
             price: item.price,
             qty: 1,
-            category: item.category
+            category: item.category,
+            subtotal: item.price * 1
         })
         newItem.save()
     };
--- models/tempCart.js
+++ models/tempCart.js
@@ -4,7 +4,9 @@ const tempCart = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     qty: { type: Number, required: true },
-    category: { type: String, required: true }
+    category: { type: String, required: true },
+    subtotal: { type: Number, required: true }
+
 });
 
 const tempUserCart = mongoose.model('tempUserCart', tempCart);
