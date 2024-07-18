fa8efbf23908841f33400cdd6163fc8feafafb38
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-07-18 17:56:38 +0800

    Re-add backup port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(express.json(), express.static('public'));
 require('./config/conf');
