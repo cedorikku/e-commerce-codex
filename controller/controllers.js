45cb1c58e001e006487c3fed3dec9bbd51c42135
xenri20 <skc1615@dlsud.edu.ph>
2024-07-18 19:11:58 +0800

    Fix the function add to cart in controller

--- controller/controllers.js
+++ controller/controllers.js
@@ -2,21 +2,20 @@ const { Inventory } = require('../models/inventory');
 const { tempUserCart } = require('../models/tempCart');
 
 const addToCart = async (req, res) => {
-    const item = await Inventory.findOne({ name: req.body.name }); 
-    const added = await Inventory.findByIdAndUpdate(item._id, { $inc: { stock: -1 } });
-    const tempItem = await tempUserCart.findOne({name: item.name})
-    .then((tempItem) => {
-        const addedItem = tempUserCart.findOneAndUpdate(tempItem._id, { $inc: { qty: 1 } });
-    })
-    .catch(tempItem => {
+    const item = await Inventory.findOne({ name: req.body.name })
+    const added = await Inventory.findByIdAndUpdate(item._id, { $inc: { stock: -1 } })
+    let tempItem = await tempUserCart.findOne({name: item.name})
+    if (tempItem) {
+        await tempUserCart.findOneAndUpdate(tempItem._id, { $inc: { qty: +1 } });
+    } else {
         const newItem = new tempUserCart({
             name: item.name,
             price: item.price,
             qty: 1,
             category: item.category
         })
-        const addedItem = newItem.save()
-    });
+        newItem.save()
+    };
 };
 
 module.exports = { addToCart };
