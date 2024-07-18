d4670481ced0e69f5e24de77f9c25eb2ac9f98db
xenri20 <skc1615@dlsud.edu.ph>
2024-07-18 18:33:46 +0800

    Made a tempcart Model

--- controller/controllers.js
+++ controller/controllers.js
@@ -3,6 +3,7 @@ const { Inventory } = require('../models/inventory');
 const addToCart = async (req, res) => {
     const item = await Inventory.findOne({ name: req.body.name }); 
     const added = await Inventory.findByIdAndUpdate(item._id, { $inc: { stock: -1 } });
+    
 };
 
 module.exports = { addToCart };
---
+++ models/tempCart.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const tempCart = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    category: { type: String, required: true }
+});
+
+const tempUserCart = mongoose.model('tempUserCart', tempCart);
+module.exports = { tempUserCart };
