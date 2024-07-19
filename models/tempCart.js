6773f0aca6dd9f7752f6aa85368386bcfba47232
xenri20 <skc1615@dlsud.edu.ph>
2024-07-19 19:03:03 +0800

    fix tempcart model

--- models/tempCart.js
+++ models/tempCart.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const tempCart = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
+    stock: { type: Number, required: true},
     qty: { type: Number, required: true },
     category: { type: String, required: true },
     subtotal: { type: Number, required: true }
