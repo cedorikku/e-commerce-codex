244db7fc1438603a23f876561738221d266ddf1d
xenri20 <skc1615@dlsud.edu.ph>
2024-07-19 19:33:52 +0800

    route the update cart module

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get('/cart', (req, res) => {
 app.get('/api/getCart', (req, res) =>  {
   controller.getCart(req,res)})
 
+app.put('/api/updateCart', (req, res) => {
+  controller.updateCart(req,res)
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
