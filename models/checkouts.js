eaf0d70ed42a2e456b945d8aa9ab3db9273a1f82
xenri20 <skc1615@dlsud.edu.ph>
2024-07-21 20:50:29 +0800

    created a schema and model for checkout

---
+++ models/checkouts.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const checkoutsSchema = new mongoose.Schema({
+    fullname: { type: String, required: true },
+    email: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    zipcode: { type: Number, required: true },
+    country: { type: String, required: true },
+    mop: { type: String, required: true },
+    sf: { type: Number, required: true },
+    initialamount: { type: Number, required: true },
+    grandtotal: { type: Number, required: true },
+    usercart: []
+});
+
+const checkouts = mongoose.model('checkouts', checkoutsSchema);
+module.exports = { checkouts };
