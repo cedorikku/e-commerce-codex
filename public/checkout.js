4238503c55ab72b61f098753a705cbe91dbb2661
xenri20 <skc1615@dlsud.edu.ph>
2024-07-21 17:10:00 +0800

    made some minor changes

--- public/checkout.js
+++ public/checkout.js
@@ -3,20 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const placeOrderButton = document.getElementById('placeOrderButton');
 
     placeOrderButton.addEventListener('click', (e) => {
-        e.preventDefault(); // Prevent the default form submission
+        e.preventDefault();
 
-        // Remove previous validation states
         form.classList.remove('was-validated');
         
-        // Check form validity
         if (form.checkValidity()) {
-            // If form is valid, show the "Order Placed" modal
             const orderPlacedModal = new bootstrap.Modal(document.getElementById('orderPlacedModal'));
             orderPlacedModal.show();
         } else {
-            // If form is invalid, trigger HTML5 form validation
-            // form.classList.
-            add('was-validated');
+            form.classList.add('was-validated');
         }
     });
 });
